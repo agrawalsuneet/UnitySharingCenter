76c0b608429156ff56a36dedb2ef1cd8bb4371dc
agrawalsuneet <agrawalsuneet@gmail.com>
2019-05-28 13:21:19 +0530

    added droid client

---
+++ DroidClient/app/src/main/java/com/agrawalsuneet/droidclient/MainActivity.java
@@ -0,0 +1,13 @@
+package com.agrawalsuneet.droidclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
