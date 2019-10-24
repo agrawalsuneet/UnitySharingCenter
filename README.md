d60002b9c00205ef25562a58f413034939444992
Suneet Agrawal <agrawalsuneet@gmail.com>
2019-10-24 13:12:27 +0530

    added native android script

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Please read
 * [Native Android screenshot/image sharing in Unity](https://medium.com/@agrawalsuneet/native-android-screenshot-image-sharing-in-unity-47eda326b80c)
 * [Native Android share to a particular app in Unity](https://medium.com/@agrawalsuneet/native-android-share-to-a-particular-app-in-unity-d73c955fe6f0)
 * [Native Android screenshot/image sharing in Unity using FileProvider](https://medium.com/@agrawalsuneet/native-android-screenshot-image-sharing-in-unity-using-fileprovider-4e1617db59c2)
+
+
+This project was last compiled with Unity 2018.4.9f1 (LTS) version
+
+
--- UnityClient/Assets/Scripts/iOS/NativeiOS.cs
+++ UnityClient/Assets/Scripts/iOS/NativeiOS.cs
@@ -5,6 +5,24 @@ using System.Runtime.InteropServices;
 
 public class NativeiOS : MonoBehaviour
 {
+
+    #region Declare external C interface    
+    #if UNITY_IOS && !UNITY_EDITOR
+
+   [DllImport("__Internal")]
+   private static extern void _nativeiOS_showAlertDialog();
+
+    #endif
+    #endregion
+
+    #region Wrapped methods and properties
+   public void showAlertDialog() {
+       #if UNITY_IOS && !UNITY_EDITOR
+       _nativeiOS_showAlertDialog();
+       #endif
+   }
+   #endregion
+
     public Button shareButton;
 
     void Start()
@@ -15,17 +33,10 @@ public class NativeiOS : MonoBehaviour
     public void OnShareButtonClick()
     {
         #if UNITY_IOS
-        _nativeiOS_showAlertDialog();
+        showAlertDialog();
         #endif
     }
 
-    #region Declare external C interface    
-    #if UNITY_IOS && !UNITY_EDITOR
-
-   [DllImport("__Internal")]
-   private static extern void _nativeiOS_showAlertDialog();
-
-    #endif
-    #endregion
+    
 
 }
